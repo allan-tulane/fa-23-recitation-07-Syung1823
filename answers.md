aa860b89b30d45e8d9d3e2bbcabe73572c67652e
Syung1823 <125316119+Syung1823@users.noreply.github.com>
2023-11-15 14:16:07 -0600

    Final Commit

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**__Simon Yung___
+**Name:**___Killian Daly_
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
@@ -9,9 +9,11 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
+The work of Reachable will be $O(n*m)$
 
 - **4)**
-
+We only need to call reachable once since if the graph is connected all nodes are reachable and len of both the orignal graph and results will be the same
 - **5)**
-
+The work is the same as Reachable since you are calling it once $O(m*n)$
 - **7)**
+Yes the work will change: in the case of the adjacency matrix we can check for neigbors in constant time instead of using a iterator. Because of this the work will be $O(m+n)$
--- main.py
+++ main.py
@@ -16,27 +16,47 @@ def reachable(graph, start_node):
     """
     result = set([start_node])
     frontier = set([start_node])
+    z = 0
+    
     while len(frontier) != 0:
-        ###TODO
-        pass
+        
+        w = result
+        for x in frontier:
+            result.update(graph[x])
+            frontier.update(graph[x])
+            
+            q = x
+            break
+        frontier.remove(q)
+        if w == result and z > 10:
+            break
+        z += 1
+        
+    
     return result
 
+    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
+    print(reachable(graph, 'A'))
 
 
 
 
 def connected(graph):
-    ### TODO
-    pass
+    reachable(graph, next(iter(graph)))
+    return len(graph) == len(reachable(graph, next(iter(graph))))
 
 
 
 
 def n_components(graph):
-    """
-    Returns:
-      the number of connected components in an undirected graph
-    """
-    ### TODO
-    pass
+    cycles = set()
+    for i in graph:
+        x = reachable(graph, next(iter(i)))
+        print(x)
+        cycles.add(len(x))
+    print(cycles)
 
+    if len(graph) in cycles:
+        return 1
+    else:
+        return len(cycles)
